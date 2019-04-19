00af46257a43b65be4d0c2676cb88ed5405f86f7
摩凝(M.Chan) <mo@lxooo.com>
2019-04-19 15:52:50 +0800

    Update init_system.sh

--- debian/init_system.sh
+++ debian/init_system.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 underline=`tput smul`
@@ -6,74 +6,98 @@ nounderline=`tput rmul`
 bold=`tput bold`
 normal=`tput sgr0`
 
-echo "--------------------------------- 升级系统 --------------------------------"
+echo -e "\n--------------------------------- 升级系统 --------------------------------"
 apt update
 apt upgrade -y
 
-echo "----------------------------- SU 免密并自动 SU -----------------------------"
-read -p "输入登陆用户的账户名：" user
-user=${user:-mchan}
+echo -e "\n--------------------------------- 主机名称 --------------------------------"
+read -p "输入主机名称：" HOSTNAME
+echo $HOSTNAME > /etc/hostname
+hostname $HOSTNAME
 
-groupadd wheel
-usermod -a -G wheel $user
-sed -i -e "s/# auth       sufficient pam_wheel.so trust/auth       sufficient pam_wheel.so trust/g" /etc/pam.d/su
-echo "su - root" >> /home/$user/.bash_profile
+echo -e "\n----------------------------- SU 免密并自动 SU -----------------------------"
+read -p "输入 SSH 登陆用户的账户名：" -t 21 USER
+USER=${USER:-root}
+KEYS="/root/.ssh/authorized_keys"
 
-echo "--------------------------- 安装基础应用和配置 ZSH --------------------------"
-apt install -y git screen autojump zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+if [ "root" != "$USER" ]; then
+    KEYS="/home/$USER/.ssh/authorized_keys"
 
-sed -i -e "s/ZSH_THEME=.*/ZSH_THEME=\"ys\"/g" ~/.zshrc
-sed -i -e "s/plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g" ~/.zshrc
-echo "" >> ~/.zshrc
-echo ". /usr/share/autojump/autojump.sh" >> ~/.zshrc
-echo "" >> ~/.zshrc
-echo "if [ -e /lib/terminfo/x/xterm-256color ]; then" >> ~/.zshrc
-echo "  export TERM='xterm-256color'" >> ~/.zshrc
-echo "else" >> ~/.zshrc
-echo "  export TERM='xterm-color'" >> ~/.zshrc
-echo "fi" >> ~/.zshrc
+    read -p "是否开启 SU 免密并自动 SU？（${underline}Yes${nounderline}/No）" -t 21 -n 1 SU
+    if [ -z "$SU" ] || [ "Y" == "$SU" ] || [ "y" == "$SU" ]; then
+        groupadd wheel
+        usermod -a -G wheel $USER
+        sed -i -e "s/# auth       sufficient pam_wheel.so trust/auth       sufficient pam_wheel.so trust/g" /etc/pam.d/su
+        echo "su - root" >> /home/$USER/.bash_profile
+    fi
+fi
 
-echo "------------------------------- 安装 Docker -------------------------------"
-read -t 7 -p "是否安装 Docker？（${underline}Yes${nounderline}/No）" doc
-if [ -z $doc || "Y" == $doc || "y" == $doc || "yes" == $doc || "Yes" == $doc ]
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXuZegS5W/Pt2fwoa550ighgACMsKG5xXOW62Um0R7YOUdtyKXuMmfKcGztCrxlxVW4/rWYikefGySmzvi8bFS/zETQW2eK5FPRVsJnzMKxXT6zniPWa38V1uxyIJ3Mr2+7YN9egVOVJ857okasoQIodvU63GtHac/iAGV7ivHz1jU1yWbnfK2fk2Gyy1XjdJEgWx71NsW1+72A/qGy+3P12zKA9IigNLYEtXcnwyYUgjrNqmGcgo/peiSQK+jYzftug5I2NLVG4IkW/l34x/wqW1beW3mAvZYLwHjZaFlgYYtULd1RePfjukfzFOASCNzXKeYguZLa/hJcOaN0Uqh m@air" >> $KEYS
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp7jaqwUsC/lrhry10C5zPww2nURKZg/WAAGDNobtPQjcE4sFCYXrh78b9pxwW1Qz1yYdoVEbh2DAXpR5Y5I1MiQ6gjiSYoyWBTBv3vl5N2o4/KWuvXd6kWu31upD9f5jZY2rEsB+hfaGSxkjEMSgBlSJmMB9cQ0AJdmUdXwhHDL1IBiahiZchqj6kDoKDcYgtdu3WI890vAz7uijHOg61EzqIG6V8MzobwwKpNQ1j2w4ea1V/bPjX+v0ybqcItYyrmqtEnZtzBtPNHn6mFmgN5y3krtMSlBV5SBkWRVSVYxtCndbbFwcB0AgkKOrXQfN6TechpGQkPeQtfYeZxgBx m@mini" >> $KEYS
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDDMrV6p/3igBXPIFxAZcKNBJyZxoKoUHknVQNZJD5gPOATNNglYpsJON3XP7Mz3vF7fh6q7tLP5Y625GXpktzO7qe2maoAKGnttjpAxCMJVUvjHx9YpCHo6jS2KmZ6AC8Gz+3+gQIDbnuUm4njovrjDpY9WA0h1bKrTpkdbR4xHw== m@6s" >> $KEYS
+
+echo -e "\n--------------------------- 安装基础应用和配置 ZSH --------------------------"
+read -p "是否安装 ZSH？（${underline}Yes${nounderline}/No）" -t 21 -n 1 ZSH
+if [ -z "$ZSH" ] || [ "Y" == "$ZSH" ] || [ "y" == "$ZSH" ]; then
+    CST=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+    apt install -y git screen autojump zsh
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $CST/plugins/zsh-syntax-highlighting
+    git clone git://github.com/zsh-users/zsh-autosuggestions $CST/plugins/zsh-autosuggestions
+
+    sed -i -e "s/ZSH_THEME=.*/ZSH_THEME=\"ys\"/g" ~/.zshrc
+    sed -i -e "s/plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g" ~/.zshrc
+    echo "" >> ~/.zshrc
+    echo ". /usr/share/autojump/autojump.sh" >> ~/.zshrc
+    echo "" >> ~/.zshrc
+    echo "if [ -e /lib/terminfo/x/xterm-256color ]; then" >> ~/.zshrc
+    echo "  export TERM='xterm-256color'" >> ~/.zshrc
+    echo "else" >> ~/.zshrc
+    echo "  export TERM='xterm-color'" >> ~/.zshrc
+    echo "fi" >> ~/.zshrc
+
+    sed -i -e "s/typeset -g ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'/typeset -g ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'/g" $CST/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+
+echo -e "\n------------------------------- 安装 Docker -------------------------------"
+read -p "是否安装 Docker？（${underline}Yes${nounderline}/No）" -t 21 -n 1 DKR
+if [ -z "$DKR" ] || [ "Y" == "$DKR" ] || [ "y" == "$DKR" ]; then
     curl -fsSL get.docker.com | sh
 fi
 
-read -t 7 -p "是否更换 Docker 为国内源？（Yes/${underline}No${nounderline}）" dcn
-if [ "Y" == $dcn || "y" == $dcn || "yes" == $dcn || "Yes" == $dcn ]
-    echo -e "\{" >> /etc/docker/daemon.json
+echo -e "\n"
+
+read -p "是否更换 Docker 为国内源？（Yes/${underline}No${nounderline}）" -t 21 -n 1 DCN
+if [ "Y" == "$DCN" ] || [ "y" == "$DCN" ]; then
+    echo -e "{" > /etc/docker/daemon.json
     echo -e "  \"registry-mirrors\": [" >> /etc/docker/daemon.json
     echo -e "    \"https://db72lygt.mirror.aliyuncs.com\"," >> /etc/docker/daemon.json
     echo -e "    \"https://registry.docker-cn.com\"" >> /etc/docker/daemon.json
     echo -e "  ]" >> /etc/docker/daemon.json
-    echo -e "\}"
+    echo -e "}"
 fi
 
-echo "---------------------------- 安装 DockerCompose ---------------------------"
-read -t 7 -p "是否安装 Docker Compose？（${underline}Yes${nounderline}/No）" com
-if [ -z $com || "Y" == $com || "y" == $com || "yes" == $com || "Yes" == $com ]
+echo -e "\n---------------------------- 安装 DockerCompose ---------------------------"
+read -p "是否安装 Docker Compose？（${underline}Yes${nounderline}/No）" -t 21 -n 1 DCP
+if [ -z "$DCP" ] || [ "Y" == "$DCP" ] || [ "y" == "$DCP" ]; then
     curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
-echo "---------------------------- 安装同步服务 LSYNCD ---------------------------"
-read -t 7 -p "是否安装 Docker Compose？（Yes/${underline}No${nounderline}）" lsd
-if [ "Y" == $lsd || "y" == $lsd || "yes" == $lsd || "Yes" == $lsd ]
+echo -e "\n---------------------------- 安装同步服务 LSYNCD ---------------------------"
+read -p "是否安装 Docker Compose？（Yes/${underline}No${nounderline}）" -t 21 -n 1 LSD
+if [ "Y" == "$LSD" ] || [ "y" == "$LSD" ]; then
     apt install lsyncd
 fi
 
-echo "--------------------------------- 生成密钥 --------------------------------"
-read -t 7 -p "是否生成密钥？（${underline}Yes${nounderline}/No）" key
-if [ -z $key || "Y" == $key || "y" == $key || "yes" == $key || "Yes" == $key ]
+echo -e "\n--------------------------------- 生成密钥 --------------------------------"
+read -p "是否生成密钥？（${underline}Yes${nounderline}/No）" -t 21 -n 1 KEY
+if [ -z "$KEY" ] || [ "Y" == "$KEY" ] || [ "y" == "$KEY" ]; then
     ssh-keygen
-fi
-
-read -t 7 -p "是否读取公钥？（${underline}Yes${nounderline}/No）" cat
-if [ -z $cat || "Y" == $cat || "y" == $cat || "yes" == $cat || "Yes" == $cat ]
+    echo ">>>>>>> 公钥 START <<<<<<<"
     cat ~/.ssh/id_rsa.pub
+    echo ">>>>>>> 公钥 END <<<<<<<"
 fi
 
-echo "------------------------------- 系统初始化完成 -----------------------------"
+echo -e "\n------------------------------- 系统初始化完成 -----------------------------"
